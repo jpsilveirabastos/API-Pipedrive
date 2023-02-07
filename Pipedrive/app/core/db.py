58492778fc63f17fae20d9cc4df17a6fd32e2590
jpsilveirabastos <74983593+jpsilveirabastos@users.noreply.github.com>
2023-02-06 21:30:23 -0300

    Update db.py

--- Pipedrive/app/core/db.py
+++ Pipedrive/app/core/db.py
@@ -1,17 +1,21 @@
 from .config import DBNAME, USER, PASSWORD, HOST
 import psycopg2
 
+
 class Db:
 
     def connect():
+        '''Connect to Postgres'''
         conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST)
         cur = conn.cursor()
         return cur,conn
 
     def disconnect(cur, conn):
+        '''Disconnect to Postgres'''
         cur.close()
         conn.close()
 
     def tables():
+        '''Table used to save the data'''
         pipedrive_m = 'pipedrive_metrics'
         return pipedrive_m
